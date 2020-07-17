32591a71c0d7fe17df3a4925c6ad0f739a7cb93c
kyeongmin92 <homme-fatale@nate.com>
2020-07-17 17:43:52 +0900

    200717

---
+++ src/chap19/MyRequest.java
@@ -0,0 +1,24 @@
+package chap19;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class MyRequest extends HttpServletRequestWrapper {
+	public MyRequest(HttpServletRequest request) {
+		super(request);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getParameter(String name) {
+		if(name.equals("name")) {
+			String n = super.getParameter("name");
+			if( n== null) {
+				return "dafault name";
+			} else {
+				return n;
+			}			
+		}
+		return super.getParameter(name);
+	}
+}
---
+++ src/chap19/NullparameterFilter.java
@@ -0,0 +1,47 @@
+package chap19;
+
+import java.io.IOException; 
+import java.util.StringTokenizer;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class NullparameterFilter
+ */
+
+public class NullparameterFilter implements Filter {
+	
+	private String[] parameterNames = null;
+		
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		NullParameterRequestWrapper requestWrapper = 
+				new NullParameterRequestWrapper((HttpServletRequest)request);
+		requestWrapper.checkNull(parameterNames);
+		
+		chain.doFilter(requestWrapper, response);
+	}
+	
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		String names = config.getInitParameter("parameterNames");
+		StringTokenizer st = new StringTokenizer(names, ",");
+		parameterNames = new String[st.countTokens()];
+		
+		for(int i =0; st.hasMoreTokens(); i++) {
+			parameterNames[i] = st.nextToken();
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+}
