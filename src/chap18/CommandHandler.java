c25fae888d837ef9280a16fa91578e8367668748
kyeongmin92 <homme-fatale@nate.com>
2020-07-24 19:13:31 +0900

    0724

---
+++ src/chap18/CommandHandler.java
@@ -0,0 +1,9 @@
+package chap18;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface CommandHandler {
+	public String process(HttpServletRequest request, 
+						HttpServletResponse response);
+}
